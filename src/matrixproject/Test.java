806570ff1be2cd08ca51f7e08b5caba9463244af
Marco <Marco@Marco-LAVORO>
2013-12-23 14:47:38 +0100

    Added author

---
+++ src/matrixproject/Test.java
@@ -0,0 +1,22 @@
+/**
+ * @author Marco
+ */
+package matrixproject;
+
+import matrixproject.fraction.Fraction;
+
+public class Test {
+    
+    public static void main(String[] args)
+    {
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(1, 2);
+        
+        System.out.println(fraction1.sum(fraction2).toString());
+        System.out.println();
+        System.out.println(fraction1.mul(fraction2).toString());
+        System.out.println();
+        System.out.println(fraction1.div(fraction2).toString());
+    }
+    
+}
